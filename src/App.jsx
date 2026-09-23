90ad599d16ca484abc65517c5b03d36ccbd02b65
Francis Yakubu <francisyakubu563@gmail.com>
2026-09-24 00:18:42 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>SellSync Dashboard</h1>
+      <p>Live and working!</p>
+    </div>
+  )
+}
+
+export default App
